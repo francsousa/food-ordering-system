a7e1695f97a9a92212bb316cbd07600b8e4a7c4c
Francisco Sousa <franciscoeds1987@hotmail.com>
2024-05-22 13:33:00 -0300

    Creating Customer Service & adjusts

---
+++ customer-service/src/main/resources/init-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('cb261a96-9a6c-4630-b4f9-aed293773056', 'user_1', 'First', 'User');
+
+INSERT INTO customer.customers(id, username, first_name, last_name)
+VALUES ('e4b3ce54-2eef-48a1-a334-6cdd2647fbb9', 'user_2', 'Second', 'User');
